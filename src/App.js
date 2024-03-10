a28c8e81af5b0b55bf9a9e27c4cd13fd56b9b3e8
alext6363 <alexthornberry9@gmail.com>
2024-03-10 00:38:54 +0000

    added code for the word and some states in app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
     <>
       <Header />
       <div className='game-container'>
         <Figure />
         <WrongLetters />
-        <Word />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
